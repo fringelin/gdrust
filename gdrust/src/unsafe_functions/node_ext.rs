694eb85c012b1a38c9f8132d3fe67dd224f3d7df
yan <3200828584@qq.com>
2022-02-19 10:22:54 +0800

    add instance

--- gdrust/src/unsafe_functions/node_ext.rs
+++ gdrust/src/unsafe_functions/node_ext.rs
@@ -1,8 +1,13 @@
 use crate::unsafe_functions::option_ext::OptionExt;
 use gdnative::api::SceneTree;
-use gdnative::prelude::{NewRef, Node, NodePath, Shared, SubClass, TRef};
+use gdnative::prelude::{NativeClass, Node, NodePath, Shared, SubClass, TInstance, TRef};
 
 pub trait NodeExt<P: Into<NodePath>> {
+    fn expect_instance<'a, T>(&self, path: P) -> Option<TInstance<'a, T>>
+    where
+        T: NativeClass,
+        T::Base: SubClass<Node>;
+
     /// Gets a typed node from a node path. This has an explicit `unsafe` block, and can panic. The
     /// unsafe code is calling `assume_safe` on the node at `path`.
     /// # Panics
@@ -40,6 +45,24 @@ pub trait NodeExt<P: Into<NodePath>> {
 }
 
 impl<T: SubClass<Node>, P: Into<NodePath>> NodeExt<P> for T {
+    fn expect_instance<'a, N>(&self, path: P) -> Option<TInstance<'a, N>>
+    where
+        N: NativeClass,
+        N::Base: SubClass<Node>,
+    {
+        let path = path.into();
+        let path_name = path.to_string();
+        unsafe {
+            self.upcast()
+                .get_node(path)
+                .godot_expect(format!("Could not find a node at {}", path_name).as_str())
+                .assume_safe()
+                .cast::<N::Base>()
+                .godot_expect("Could not cast")
+                .cast_instance()
+        }
+    }
+
     fn expect_node<'a, Child>(&self, path: P) -> TRef<'a, Child>
     where
         Child: SubClass<Node>,
